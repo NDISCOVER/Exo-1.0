11ec4ff92ddbf65fc413727930604316bdb08b9b
Marc Foley <m.foley.88@gmail.com>
2017-02-23 16:49:06 +0000

    sources | fonts: dehinted fonts to match previous release

--- sources/build.sh
+++ sources/build.sh
@@ -7,7 +7,7 @@ FONTS=$(ls *.ttf)
 for font in $FONTS
 do 
     echo autohinting $font
-    ttfautohint -x 0 $font $font.fix
+    ttfautohint -d $font $font.fix
     rm $font
     mv $font.fix $font
 done
